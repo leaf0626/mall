7a95e4a4a0ada46330fc16c77515b122d33897b5
hongye <956734033@qq.com>
2021-03-20 13:42:33 +0800

    Message:管理文件夹

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    <!-- <router-view/> -->
-    <!-- <div id="tab"> -->
-      <!-- <router-link to="/">Home</router-link> |
+    <router-view/>
+    <div id="tab">
+      111
+      <!-- <router-link to="/">Home</router-link> 
       <router-link to="/about">About</router-link> -->
-    <!-- </div> -->
-    11122
+    </div>
   </div>
 </template>
 
-<style lang="less">
+<script>
+
+export default {
+  name:'app'
+}
+
+</script>
+
+<style>
+@import "~@/assets/style/base.css";
 </style>
